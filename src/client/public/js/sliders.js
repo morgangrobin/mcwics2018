ac357362e9afdcd3f73241bea82a958aaf57e4fd
Susan Ding <Susan@Susans-MacBook-Air.local>
2018-03-11 04:34:20 -0400

    Updates to sliders

---
+++ src/client/public/js/sliders.js
@@ -0,0 +1,11 @@
+var numClasses = 2;
+
+window.onLoad(){
+	document.getElementById("myRange").val() = 50;
+
+}
+
+
+var slider1value = document.getElementById("myRange").val();
+var slider2value = document.getElementById("myRange2").val();
+
